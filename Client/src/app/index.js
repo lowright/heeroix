8bcba7346db538b744569deaf1e9e9bb76a857d4
andrew <artmyweb@gmail.com>
2021-02-13 16:31:41 +0200

    Initial Commit with Client generation

---
+++ Client/src/app/index.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+import React from "react";
+
+
+class App extends Component {
+
+    render( ) {
+
+        return (
+            <p>Heeroix</p>
+        );
+
+    }
+
+}
+
+export { App };
